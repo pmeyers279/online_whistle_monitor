678628e323c1efd59e2e1fb327b55f4bbd1e4379
meyers <meyers@ldas-pcdev1.ligo-la.caltech.edu>
2015-11-02 19:05:01 -0600

    updates that simplify code and allow it to actually read from frames

--- histogram/trigger_functions.py
+++ histogram/trigger_functions.py
@@ -9,6 +9,7 @@ from gwpy.segments import Segment
 from gwpy.plotter import HistogramPlot
 from glue.ligolw import table
 from vco_functions import *
+import matplotlib.pyplot as plt
 import markup
 
 
@@ -21,42 +22,36 @@ def plot_name(channel, start, end, tag, format='png'):
     return '%s_%s-%d-%d.%s' % (c, tag, s, d, format)
 
 
-def get_omicron_files(ifo, segment, trig_dir=None, channel='GDS-CALIB_STRAIN'):
-    if trig_dir is not None:
-        trig_dir = '/home/detchar/triggers/O1/%s/%s_Omicron' % (ifo, channel)
-    st = segment[0]
-    et = segment[1]
-    dir1 = str(st)[0:5]
-    dirend = str(et)[0:5]
-    dirs = np.arange(int(dir1), int(dirend) + 1)
-    files = []
-    for directory in dirs:
-        #print('looking for trigs in %s/%d' % (trig_dir, directory))
-        files_to_add = sorted(
-            glob.glob('%s/%d/*.xml.gz' % (trig_dir, directory)))
-	for f in files_to_add:
-            files.append(f)
-    return files
-
-
-def get_all_vco_triggers(omicron_files,seg,frames=False, fit=True,
+def get_all_vco_triggers(ifo, seg, frames=False, fit=True,
                          channel='GDS-CALIB_STRAIN'):
+    st = int(seg[0])
+    et = int(seg[1])
+    st2 = st
+    First = 1
+    # break into 1000s, run into memory issues otherwise
     amps = []
-    for f in omicron_files:
-	f2 = f.split('/')[-1]
-        # parse file name
-        ifo = f2.split('-')[0]
-        st = int(f2.split('-')[2])
-        dur = int(f2.split('-')[3].split('.')[0])
-        et = st + dur
-	if st > int(seg[0]) and et < int(seg[1]):
-            # generate fast vco
-            vco = generate_fast_vco(ifo, Segment(st, et), frames=0, fit=fit)
-            trigs, t, s = read_omicron_trigs(f, Segment(st, et))
-            vtrigs = get_vco_trigs(vco, t, channel=channel)
-            for vtrig in vtrigs:
-                amps.append(vtrig)
-    return amps
+    while (et > st2):
+	dur = min(et - st2, 1000)
+        vco = generate_fast_vco(ifo, Segment(st2, st2+dur), frames=frames, fit=fit)
+        trigs = SnglBurstTable.fetch("%s:%s" % (ifo, channel), 'omicron', st2, st2+dur,
+				 filt=lambda x: st2 <= x.get_peak() < st2+dur)
+        if First:
+	    trigs2 = table.new_from_template(trigs)
+	    First=0
+	trigs2.extend(trigs)
+	vtrigs = get_vco_trigs(vco, trigs, channel=channel)
+	for vtrig in vtrigs:
+	    amps.append(vtrig)
+	st2 += 1000
+
+    central_freqs = trigs2.get_column('peak_frequency')
+    snrs = trigs2.get_column('snr')
+    
+    amps = np.asarray(amps)
+    amps = amps[~(np.isnan(amps))]
+    central_freqs = central_freqs[~(np.isnan(amps))]
+    snrs = snrs[~(np.isnan(amps))]
+    return amps, central_freqs, snrs
 
 
 def plot_vco_hist(vco_trigs, segment, channel):
@@ -67,61 +62,37 @@ def plot_vco_hist(vco_trigs, segment, channel):
         vco_trigs, weights=1. / abs(int(end) - int(start)), bins=100, log=True)
     plot.set_xlabel('VCO Frequency [kHz from 79 MHz]')
     plot.set_ylabel('Rate [Hz]')
-    plot.set_title(channel.replace('_', '\_') + ' omicron glitch histogram')
+    plot.set_title(channel.replace('_', '\_') + ' omicron glitch histogram', fontsize=14)
     plot.suptitle(str(segment[0]) + '-' + str(segment[-1]))
     plot.savefig(png)
     plot.close()
-    #print('%s written' % png)
+    print('%s written' % png)
+
+def plot_omicron_scatter(vco_trigs, central_freqs, snrs, segment, channel):
+    snrs[np.where(snrs>20)] = 20
+    start = segment[0]
+    end = segment[1]
+    png = plot_name(channel, start, end, 'OMICRON-SCATTER', format='png')
+    plt.scatter(vco_trigs, central_freqs, c=snrs)
+    plt.xlabel('VCO Frequency [kHz from 79 MHz]', fontsize=12)
+    plt.ylabel('Omicron central frequency [Hz]', fontsize=12)
+    plt.title('%s Omicron Scatter' % channel.replace('_','\_'), fontsize=14)
+    plt.suptitle('%d-%d' % (start, end))
+    ax = plt.gca()
+    ax.set_ylim(0,8192)
+    plt.savefig(png)
 
 
-def get_vco_trigs(vco, times, channel='GDS-CALIB_STRAIN'):
+def get_vco_trigs(vco, trigs, channel='GDS-CALIB_STRAIN'):
     #print('Finding mean values of %s' % channel)
-    s = times.size
-    amp = np.zeros(s)
+    times = trigs.get_peak()
+    amp = np.zeros(len(trigs))
     for i, t in enumerate(times):
         t = t.seconds + t.nanoseconds / 1e9
         idx1 = int(
-            (t - vco.times.value[0] - 0.01) * vco.sample_rate.value)
+            (t - vco.times.value[0] - 0.1) * vco.sample_rate.value)
         idx2 = int(
-            (t - vco.times.value[0] + 0.01) * vco.sample_rate.value)
+            (t - vco.times.value[0] + 0.1) * vco.sample_rate.value)
         temp = vco[idx1:idx2]
         amp[i] = ((temp.mean().value) - 79000000) / 1.e3
-    #    print('    Processed trigger %d/%d' % (i + 1, s), end='\r')
-    #print('    Processed trigger %d/%d' % (s, s))
     return amp
-
-
-def read_omicron_trigs(omicron_files, segment):
-    """
-    """
-    if isinstance(omicron_files, list):
-	##print('HI THERE PAT. FUCK YOU!!!')
-    	trigs1 = SnglBurstTable.read(omicron_files[0], format='ligolw')
-    else:
-	# i'm being lazy so i don't have to rewrite code below
-	omicron_files = [omicron_files]
-    	trigs1 = SnglBurstTable.read(omicron_files, format='ligolw')
-    trigs = table.new_from_template(trigs1)
-    for omicron_file in omicron_files:
-        trigs2 = table.new_from_template(trigs1)
-        trigs2 = SnglBurstTable.read(omicron_file, format='ligolw')
-        trigs.extend(filter(lambda row: row.get_peak() in segment, trigs2))
-    times = []
-    #print('%d found' % len(trigs))
-    times = trigs.get_peak()
-    s = times.size
-    return trigs, times, s
-
-
-def make_html_page(segs, channel='L1:GDS-CALIB_STRAIN'):
-    channel = channel.replace(':', '-')
-    page = markup.page()
-    page.init(title="VCO Histogram to Search for Whistles in %s" % channel)
-    page.div()
-    for seg in segs:
-        page.div()
-        page.img(src="%s_VCO_HIST-%d-%d.png" %
-                 (channel, int(seg[0]), int(seg[1])))
-        page.p("VCO Histogram")
-        page.div.close()
-    page.div.close()
